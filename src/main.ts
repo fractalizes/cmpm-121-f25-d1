78fa0f98b88cf20e35946cc378b50c6d26c1e8ef
fractalizes <149290799+fractalizes@users.noreply.github.com>
2025-10-08 01:32:33 +0000

    (step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+let counter: number = 0;
+
 document.body.innerHTML = `
+  <p>Clicks: <span id="counter">0</span></p>
+  <button id="increment">🤯</button>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  counter++;
+  counterElement.innerHTML = counter.toString();
+});
