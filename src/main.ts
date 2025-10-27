94cf7ba46880feda1581fa5ff46072359c04c707
fractalizes <149290799+fractalizes@users.noreply.github.com>
2025-10-27 10:35:39 +0000

    moved everything into a main() function that only loads when dom is finished loading

--- src/main.ts
+++ src/main.ts
@@ -42,161 +42,166 @@ document.body.innerHTML = `
 `;
 document.body.style.backgroundColor = "#0256a6";
 
-// ugprade definitions
-const upgradeA: Upgrade = {
-  icon: "🎣",
-  text: "Super Rod",
-  flavor: '"Upgrade your trusty rod!"',
-  button: document.getElementById("rod-upgrade")! as HTMLButtonElement,
-  purchased: false,
-  quantity: 0,
-  cost: 10,
-  rate: 0.1,
-};
-const upgradeB: Upgrade = {
-  icon: "🪱",
-  text: "Fish Bait",
-  flavor: '"Bait so good all the fish want them!"',
-  button: document.getElementById("bait-upgrade")! as HTMLButtonElement,
-  purchased: false,
-  quantity: 0,
-  cost: 100,
-  rate: 2,
-};
-const upgradeC: Upgrade = {
-  icon: "🕸️",
-  text: "Fishing Net",
-  flavor: '"Why catch one when you can catch multiple?"',
-  button: document.getElementById("net-upgrade")! as HTMLButtonElement,
-  purchased: false,
-  quantity: 0,
-  cost: 1000,
-  rate: 50,
-};
-const upgradeD: Upgrade = {
-  icon: "🧍",
-  text: "Fisherman Fran",
-  flavor: '"You can always put your trust into Fran"',
-  button: document.getElementById("fran-upgrade")! as HTMLButtonElement,
-  purchased: false,
-  quantity: 0,
-  cost: 10000,
-  rate: 100,
-};
-const upgradeE: Upgrade = {
-  icon: "⚓",
-  text: "Better Ship",
-  flavor: '"Travel the ocean and seas!"',
-  button: document.getElementById("ship-upgrade")! as HTMLButtonElement,
-  purchased: false,
-  quantity: 0,
-  cost: 25000,
-  rate: 500,
-};
+function main() {
+  // core functions
+  function update() {
+    calculateTime();
+    autoCounter();
+    animateClicker();
+    updateAssets();
 
-// game state variables
-const upgrades: Upgrade[] = [
-  upgradeA,
-  upgradeB,
-  upgradeC,
-  upgradeD,
-  upgradeE,
-];
-const time: number[] = [];
-let timePassed: number = 0;
-let counter: number = 0;
-let rate: number = 0;
+    // recursion to continue update going
+    requestAnimationFrame(update);
+  }
 
-// button animation variables
-const buttonMin: number = 1;
-const buttonMax: number = 1.25;
-const buttonCooldown: number = 500;
-let timeSinceLastAnimation: number = 0;
-let currentButtonScale: number = buttonMin;
-let buttonScaleRate: number = 0.0025;
-
-// dom elements
-const button = document.getElementById("increment")!;
-const counterElement = document.getElementById("counter")!;
-const rateElement = document.getElementById("rate")!;
-
-// core functions
-function update() {
-  calculateTime();
-  autoCounter();
-  animateClicker();
-  updateAssets();
-
-  // recursion to continue update going
-  requestAnimationFrame(update);
-}
-
-function autoCounter() {
-  /*rate = (upgradeA.purchased ? upgradeA.rate : 0) +
+  function autoCounter() {
+    /*rate = (upgradeA.purchased ? upgradeA.rate : 0) +
     (upgradeB.purchased ? upgradeB.rate : 0) +
     (upgradeC.purchased ? upgradeB.rate : 0);*/
-  counter += rate * timePassed / 10000;
-}
+    counter += rate * timePassed / 10000;
+  }
 
-// measures time in seconds, not milliseconds
-function calculateTime() {
-  time.unshift(performance.now());
-  if (time.length > 10) {
-    const prev = time.pop();
-    if (prev !== undefined) {
-      timePassed = performance.now() - prev;
+  // measures time in seconds, not milliseconds
+  function calculateTime() {
+    time.unshift(performance.now());
+    if (time.length > 10) {
+      const prev = time.pop();
+      if (prev !== undefined) {
+        timePassed = performance.now() - prev;
+      }
     }
   }
-}
 
-function animateClicker() {
-  if (timeSinceLastAnimation >= buttonCooldown) {
-    if (currentButtonScale > buttonMax || currentButtonScale < buttonMin) {
-      buttonScaleRate = -buttonScaleRate;
+  function animateClicker() {
+    if (timeSinceLastAnimation >= buttonCooldown) {
+      if (currentButtonScale > buttonMax || currentButtonScale < buttonMin) {
+        buttonScaleRate = -buttonScaleRate;
+      }
+      currentButtonScale = currentButtonScale + buttonScaleRate;
+      button.style.transform = "scale(" + currentButtonScale.toString() + ")";
+      timeSinceLastAnimation = 0;
     }
-    currentButtonScale = currentButtonScale + buttonScaleRate;
-    button.style.transform = "scale(" + currentButtonScale.toString() + ")";
-    timeSinceLastAnimation = 0;
+    timeSinceLastAnimation += timePassed;
   }
-  timeSinceLastAnimation += timePassed;
-}
 
-function updateAssets() {
+  function updateAssets() {
+    upgrades.forEach((upgrade) => {
+      upgrade.button.style.background = counter >= upgrade.cost
+        ? "#ffffff9e"
+        : "#ffffff42";
+      upgrade.button.innerHTML = upgrade.icon + upgrade.text + " x" +
+        upgrade.quantity + "<br>[ $" + upgrade.cost.toFixed(2) + " ~ $" +
+        upgrade.rate.toFixed(2) + "/sec ]<br><br>" + upgrade.flavor;
+    });
+
+    counterElement.innerHTML = counter.toFixed(2);
+    rateElement.innerHTML = rate.toFixed(2);
+  }
+
+  // ugprade definitions
+  const upgradeA: Upgrade = {
+    icon: "🎣",
+    text: "Super Rod",
+    flavor: '"Upgrade your trusty rod!"',
+    button: document.getElementById("rod-upgrade")! as HTMLButtonElement,
+    purchased: false,
+    quantity: 0,
+    cost: 10,
+    rate: 0.1,
+  };
+  const upgradeB: Upgrade = {
+    icon: "🪱",
+    text: "Fish Bait",
+    flavor: '"Bait so good all the fish want them!"',
+    button: document.getElementById("bait-upgrade")! as HTMLButtonElement,
+    purchased: false,
+    quantity: 0,
+    cost: 100,
+    rate: 2,
+  };
+  const upgradeC: Upgrade = {
+    icon: "🕸️",
+    text: "Fishing Net",
+    flavor: '"Why catch one when you can catch multiple?"',
+    button: document.getElementById("net-upgrade")! as HTMLButtonElement,
+    purchased: false,
+    quantity: 0,
+    cost: 1000,
+    rate: 50,
+  };
+  const upgradeD: Upgrade = {
+    icon: "🧍",
+    text: "Fisherman Fran",
+    flavor: '"You can always put your trust into Fran"',
+    button: document.getElementById("fran-upgrade")! as HTMLButtonElement,
+    purchased: false,
+    quantity: 0,
+    cost: 10000,
+    rate: 100,
+  };
+  const upgradeE: Upgrade = {
+    icon: "⚓",
+    text: "Better Ship",
+    flavor: '"Travel the ocean and seas!"',
+    button: document.getElementById("ship-upgrade")! as HTMLButtonElement,
+    purchased: false,
+    quantity: 0,
+    cost: 25000,
+    rate: 500,
+  };
+
+  // game state variables
+  const upgrades: Upgrade[] = [
+    upgradeA,
+    upgradeB,
+    upgradeC,
+    upgradeD,
+    upgradeE,
+  ];
+  const time: number[] = [];
+  let timePassed: number = 0;
+  let counter: number = 0;
+  let rate: number = 0;
+
+  // button animation variables
+  const buttonMin: number = 1;
+  const buttonMax: number = 1.25;
+  const buttonCooldown: number = 500;
+  let timeSinceLastAnimation: number = 0;
+  let currentButtonScale: number = buttonMin;
+  let buttonScaleRate: number = 0.0025;
+
+  // dom elements
+  const button = document.getElementById("increment")!;
+  const counterElement = document.getElementById("counter")!;
+  const rateElement = document.getElementById("rate")!;
+
+  // clicker event listener
+  button.addEventListener("click", () => {
+    counter++;
+  });
+
   upgrades.forEach((upgrade) => {
-    upgrade.button.style.background = counter >= upgrade.cost
-      ? "#ffffff9e"
-      : "#ffffff42";
-    upgrade.button.innerHTML = upgrade.icon + upgrade.text + " x" +
-      upgrade.quantity + "<br>[ $" + upgrade.cost.toFixed(2) + " ~ $" +
-      upgrade.rate.toFixed(2) + "/sec ]<br><br>" + upgrade.flavor;
+    rate = 0;
+    // upgrade event listener
+    upgrade.button.addEventListener("click", () => {
+      if (counter >= upgrade.cost) {
+        if (!upgrade.purchased) upgrade.purchased = true;
+        rate += upgrade.rate;
+
+        upgrade.rate += upgrade.rate;
+        counter -= upgrade.cost;
+        upgrade.cost *= upgrade.quantity >= 10
+          ? (upgrade.quantity >= 25 ? 1.75 : 1.5)
+          : 1.25;
+        upgrade.quantity++;
+      }
+    });
   });
 
-  counterElement.innerHTML = counter.toFixed(2);
-  rateElement.innerHTML = rate.toFixed(2);
+  // game loop initialization
+  update();
 }
 
-// clicker event listener
-button.addEventListener("click", () => {
-  counter++;
-});
-
-upgrades.forEach((upgrade) => {
-  rate = 0;
-  // upgrade event listener
-  upgrade.button.addEventListener("click", () => {
-    if (counter >= upgrade.cost) {
-      if (!upgrade.purchased) upgrade.purchased = true;
-      rate += upgrade.rate;
-
-      upgrade.rate += upgrade.rate;
-      counter -= upgrade.cost;
-      upgrade.cost *= upgrade.quantity >= 10
-        ? (upgrade.quantity >= 25 ? 1.75 : 1.5)
-        : 1.25;
-      upgrade.quantity++;
-    }
-  });
-});
-
-// game loop initialization
-update();
+// load everything when dom is finished loading
+document.addEventListener("DOMContentLoaded", main);
